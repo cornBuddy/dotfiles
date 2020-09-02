f1b130398e4ba444b6fb59315fd7f36ecf09fc66
Heorhi Sakovich <heorhi.sakovich@gmail.com>
2020-09-02 18:02:09 +0300

    move xorg and suckless to role

--- deploy.sh
+++ deploy.sh
@@ -2,17 +2,6 @@
 
 set -e
 
-echo build suckless soft
-for dir in ./suckless/*
-do
-    sudo make install -C "${dir}"
-    sudo make clean -C "${dir}"
-done
-
-echo config xinit
-ln -sf "$(pwd)/xinitrc" ~/.xinitrc
-mkdir -p ~/.config/xkb
-
 echo set up peripheral devices
 ln -sf "$(pwd)/xkbconfig" ~/.config/xkb/my
 sudo ln -sf $(pwd)/xorg/* /etc/X11/xorg.conf.d
