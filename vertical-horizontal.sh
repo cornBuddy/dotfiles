10f415ab492f57731a0558d67ac9e82418eea4e1
Heorhi Sakovich <heorhi.sakovich@gmail.com>
2020-09-02 16:59:16 +0300

    some changes

--- vertical-horizontal.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-xrandr --output HDMI1 --primary --mode 1920x1080 --pos 1080x416 --rotate normal --output VIRTUAL1 --off --output DP1 --off --output VGA1 --mode 1920x1080 --pos 0x0 --rotate left
