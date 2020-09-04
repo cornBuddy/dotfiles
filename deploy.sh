282b5cb5aa29237aa52651d250845373021ecc8d
Heorhi Sakovich <heorhi.sakovich@gmail.com>
2020-09-04 18:21:53 +0300

    finish ansible migration

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-echo set up peripheral devices
-sudo ln -sf $(pwd)/xorg/* /etc/X11/xorg.conf.d
-
-echo configuring systemd
-sudo ln -sf $(pwd)/systemd/logind.conf /etc/systemd/logind.conf
